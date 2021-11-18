a70682e8da978b26f33dbdc0ad89d61cb21b5ffc
bytesx <pnada.black.edg@gmail.com>
2021-11-18 14:09:06 -0600

    Creating new Webhook

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,11 +3,6 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo desde Java");
-        System.out.println("Probando Jenkins");
-        System.out.println("JavaC Build and Execution");
-        System.out.println("Testing new function with Shell fixed");
-        System.out.println("Probando de nuevo Jenkins 2");
-
+        System.out.println("Creating Automation in Jenkins and Java Project");
     }
 }
