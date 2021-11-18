caa3219d0554e03b21ac9c98f7a2cead6fca9098
bytesx <pnada.black.edg@gmail.com>
2021-11-17 20:44:17 -0600

    New Dev Branch

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo desde Java");
         System.out.println("Probando Jenkins");
         System.out.println("JavaC Build and Execution");
         System.out.println("Testing new function with Shell fixed");
