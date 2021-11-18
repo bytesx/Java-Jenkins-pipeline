b5eb34732acd465b968800d2877cc8fb20af8b7a
bytesx <pnada.black.edg@gmail.com>
2021-11-18 15:18:24 -0600

    Enabiling Catlight

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,6 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Creating Automation in Jenkins and Java Project")
+        System.out.println("Creating Automation in Jenkins and Java Project");
     }
 }
